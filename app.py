d496c0451551c7a2f44556f43c23c4b834802ec7
Hilarie Sit <hs764@cornell.edu>
2022-11-11 11:19:30 -0500

    match socket versions

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from flask_socketio import SocketIO, emit
 import os
 
 app = Flask(__name__)
-socketio = SocketIO(app, cors_allowed_origins="*")
+socketio = SocketIO(app, cors_allowed_origins="https://dreamworthie.com")
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
